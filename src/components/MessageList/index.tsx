4e89266ad58446f3f0319dc3bac32aeb7008335d
Mhayk Whandson <hi@mhayk.com>
2021-10-21 17:26:54 +0100

    it is being showed messages from our backend.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,42 +1,44 @@
+import { useEffect, useState } from 'react'
+import { api } from '../../services/api'
+
 import styles from './styles.module.scss'
 
 import logoImg from '../../../assets/logo.svg'
 
+type Message = {
+    id: string;
+    text: string;
+    user: {
+        name: string;
+        avatar_url: string;
+    }
+}
+
 export function MessageList() {
+    const [messages, setMessages] = useState<Message[]>([])
+
+    useEffect(() => {
+        api.get<Message[]>('/messages/last3').then(response => {
+            setMessages(response.data)
+        })
+    }, [])
+
     return (
         <div className={styles.messageListWrapper}>
             <img src={logoImg} alt="DoWhile 2021" />
 
             <ul className={styles.messageList}>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>I am looking forward to start this event, of course it will be the best one.</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/mhayk.png" alt="Mhayk Whandson" />
+                {messages.map(message => (
+                    <li className={styles.message} key={message.id}>
+                        <p className={styles.messageContent}>{message.text}</p>
+                        <div className={styles.messageUser}>
+                            <div className={styles.userImage}>
+                                <img src={message.user.avatar_url} alt="Mhayk Whandson" />
+                            </div>
+                            <span>{message.user.name}</span>
                         </div>
-                        <span>Mhayk Whandson</span>
-                    </div>
-                </li>
-
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>I am looking forward to start this event, of course it will be the best one.</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/mhayk.png" alt="Mhayk Whandson" />
-                        </div>
-                        <span>Mhayk Whandson</span>
-                    </div>
-                </li>
-
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>I am looking forward to start this event, of course it will be the best one.</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/mhayk.png" alt="Mhayk Whandson" />
-                        </div>
-                        <span>Mhayk Whandson</span>
-                    </div>
-                </li>
+                    </li>
+                ))}
             </ul>
         </div>
     )
