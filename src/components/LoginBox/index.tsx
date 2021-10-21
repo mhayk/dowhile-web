15649467575a373db3632cba7082cc2b25fbe0e2
Mhayk Whandson <hi@mhayk.com>
2021-10-21 23:21:38 +0100

    The AuthContext has been used via useContext hook.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,11 +1,15 @@
 import styles from './styles.module.scss'
 import { VscGithubInverted } from 'react-icons/vsc'
+import { useContext } from 'react'
+import { AuthContext } from '../../contexts/auth'
 
 export function LoginBox() {
+    const { signInUrl, user } = useContext(AuthContext)
+    console.log(user)
     return (
         <div className={styles.loginBoxWrapper}>
             <strong>Enter and share your message</strong>
-            <a href="" className={styles.signInWithGithub}>
+            <a href={signInUrl} className={styles.signInWithGithub}>
                 <VscGithubInverted size="24" />
                 Sign in with Github
             </a>
