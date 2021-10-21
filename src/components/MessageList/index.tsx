f15cbfead21c4ddba411d9347de3c81119dadb27
Mhayk Whandson <hi@mhayk.com>
2021-10-21 11:43:56 +0100

    MessageList component has been styled.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,7 +1,43 @@
+import styles from './styles.module.scss'
+
+import logoImg from '../../../assets/logo.svg'
+
 export function MessageList() {
     return (
-        <div>
-            <h1>MessageList</h1>
+        <div className={styles.messageListWrapper}>
+            <img src={logoImg} alt="DoWhile 2021" />
+
+            <ul className={styles.messageList}>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>I am looking forward to start this event, of course it will be the best one.</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/mhayk.png" alt="Mhayk Whandson" />
+                        </div>
+                        <span>Mhayk Whandson</span>
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>I am looking forward to start this event, of course it will be the best one.</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/mhayk.png" alt="Mhayk Whandson" />
+                        </div>
+                        <span>Mhayk Whandson</span>
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>I am looking forward to start this event, of course it will be the best one.</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/mhayk.png" alt="Mhayk Whandson" />
+                        </div>
+                        <span>Mhayk Whandson</span>
+                    </div>
+                </li>
+            </ul>
         </div>
     )
 }
