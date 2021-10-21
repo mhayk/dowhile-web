79174d03904ff660c81a4f8701e959d19dd5df8c
Mhayk Whandson <hi@mhayk.com>
2021-10-21 11:10:28 +0100

    LoginBox component has been styled.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,7 +1,14 @@
+import styles from './styles.module.scss'
+import { VscGithubInverted } from 'react-icons/vsc'
+
 export function LoginBox() {
     return (
-        <div>
-            <h1>LoginBox</h1>
+        <div className={styles.loginBoxWrapper}>
+            <strong>Enter and share your message</strong>
+            <a href="" className={styles.signInWithGithub}>
+                <VscGithubInverted size="24" />
+                Sign in with Github
+            </a>
         </div>
     )
 }
